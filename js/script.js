7f11270b1405d5bd6e46d167a3d92f7e4bdaf8aa
Erzhan <erzhik68@mail.ru>
2023-05-02 09:06:17 +0500

    Timer updated

--- js/script.js
+++ js/script.js
@@ -32,4 +32,63 @@ window.addEventListener("DOMContentLoaded", function () {
       }
     }
   });
+
+  // Timer
+
+  let deadline = "2023-05-04  GMT+5";
+  console.log(deadline);
+  console.log(new Date());
+  console.log(Date.parse(deadline));
+  console.log(Date.parse("2023-05-04"));
+  console.log(Date.parse("2023-05-04 GMT+5"));
+  console.log(Date.parse(new Date()));
+
+  function getTimeRemaining(endtime) {
+    let t = Date.parse(endtime) - Date.parse(new Date()),
+      seconds = Math.floor((t / 1000) % 60),
+      minutes = Math.floor((t / 1000 / 60) % 60),
+      hours = Math.floor(t / (1000 * 60 * 60));
+    // hours = Math.floor((t / 1000 / 60 / 60) % 24),
+    // days = Math.floor((t / (1000 * 60 * 60 * 24)));
+    //console.log("t: ", t);
+
+    return {
+      total: t,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
+    };
+  }
+
+  function setClock(id, endtime) {
+    let timer = document.getElementById(id),
+      hours = timer.querySelector(".hours"),
+      minutes = timer.querySelector(".minutes"),
+      seconds = timer.querySelector(".seconds"),
+      timeInterval = setInterval(updateClock, 1000);
+
+    function updateClock() {
+      let t = getTimeRemaining(endtime);
+
+      function addZero(num) {
+        if (num < 10) {
+          return "0" + num;
+        }
+        return num;
+      }
+
+      hours.textContent = addZero(t.hours);
+      minutes.textContent = addZero(t.minutes);
+      seconds.textContent = addZero(t.seconds);
+
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+        hours.textContent = "00";
+        minutes.textContent = "00";
+        seconds.textContent = "00";
+      }
+    }
+  }
+
+  setClock("timer", deadline);
 });
